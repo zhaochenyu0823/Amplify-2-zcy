ef824c0ca398ef3a9a5534554346fd777be80d85
WEL-ADNET\C_ZHAO <Chenyu_Zhao-p@wellnet.co.jp>
2024-05-24 15:28:54 +0900

    "Access-Control-Allow-Origin": "*",

--- amplify/functions/say-hello-zcy/handler.ts
+++ amplify/functions/say-hello-zcy/handler.ts
@@ -6,8 +6,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     statusCode: 200,
     // Modify the CORS settings below to match your specific requirements
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:5173/", // Restrict this to domains you trust
-      "Access-Control-Allow-Headers": "Content-Type", // Specify only the headers you need to allow
+      "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
+      "Access-Control-Allow-Headers": "*", // Specify only the headers you need to allow
     },
     body: JSON.stringify("Hello from zcy-api-function!"),
   };
